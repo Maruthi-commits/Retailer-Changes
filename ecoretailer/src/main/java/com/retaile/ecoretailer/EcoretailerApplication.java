6698309acd904695e1e690777f23c1234abf1be5
Maruthi-commits <maruthikalakuntla@gmail.com>
2024-12-18 16:12:39 +0530

    Add files via upload
    Added code changes for Retailer project

---
+++ ecoretailer/src/main/java/com/retaile/ecoretailer/EcoretailerApplication.java
@@ -0,0 +1,13 @@
+package com.retaile.ecoretailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoretailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoretailerApplication.class, args);
+	}
+
+}
---
+++ ecoretailer/src/test/java/com/retaile/ecoretailer/EcoretailerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retaile.ecoretailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoretailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
